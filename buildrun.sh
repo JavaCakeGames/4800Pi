f113cfea77b76d7a770e83fe4140dc7bf89eb05e
JavaCakeGames <86204652+JavaCakeGames@users.noreply.github.com>
2025-01-04 16:31:34 +0000

    Update year patch

--- buildrun.sh
+++ buildrun.sh
@@ -3,12 +3,12 @@
 java -jar prog8compiler-10.2-all.jar -target cx16 pi.p8
 sed -i "/jsr  cx16.init_system_phase2/d" pi.asm
 sed -i "/jmp  cx16.cleanup_at_exit/d" pi.asm
-sed -ie "s/.word  (+), 2024/.word  (+), 0/" pi.asm
+sed -ie "s/.word  (+), 2025/.word  (+), 0/" pi.asm
 sed -ie "s/, \$3a, \$8f, '\ prog8'//" pi.asm
 
 sed -i "/jsr  sys.init_system_phase2/d" pi.asm
 sed -i "/jmp  sys.cleanup_at_exit/d" pi.asm
-sed -ie "s/.word  (+), 2024/.word  (+), 0/" pi.asm
+sed -ie "s/.word  (+), 2025/.word  (+), 0/" pi.asm
 sed -ie "s/, \$3a, \$8f, '\ prog8'//" pi.asm
 sed -i "/src line:/d" pi.asm
 sed -i "/; program startup initialization/,/; statements/d" pi.asm
